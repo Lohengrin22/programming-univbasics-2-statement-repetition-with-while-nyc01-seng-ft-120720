a59dca8aa7a37bc22c1164aad5b404544299ddc6
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-11-17 19:02:09 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,9 @@
-# Write your code here
+HNY = "Happy New Year!"
+count = 10 
+while count >= 1 && count != HNY 
+do 
+  puts "#{count}"
+  count -= 1 
+end
+puts "Happy New Year!"
+  # Write your code here
